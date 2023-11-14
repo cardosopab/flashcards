126a0f1e9c439fb34feaca7f58af96915368dc02
Pablo <cardosopab@gmail.com>
2023-11-13 19:39:11 -0600

    init server

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response(Bun.file("./index.html"));
+    if (url.pathname === "/card") return new Response("Card!");
+    return new Response("404!");
+  },
+  port: 8080,
+});
